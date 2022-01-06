62f419b0c6d5bee7b3389c6718fd92b4f123ed8b
Kajal Patel <92489865+13kajal@users.noreply.github.com>
2022-01-06 12:25:18 -0600

    added files

---
+++ src/test/java/com/pratice/myfirstjava/MyfirstjavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pratice.myfirstjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyfirstjavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
